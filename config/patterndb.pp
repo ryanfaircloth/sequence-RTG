d7cc4421a4c5de7a6294b8286ed31f9285e1e2f0
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <git@faxmodem.org>
2019-04-16 14:33:46 +0200

    tweak

--- config/patterndb.pp
+++ config/patterndb.pp
@@ -6,14 +6,14 @@ class { 'patterndb':
 
 patterndb::simple::ruleset { 'promoted':
   id => '8dffd37a-0602-4ba8-88a5-cf5490dbf629',
-  patterns => ['promoted'],
+  patterns => ['sshd'],
   pubdate => '2019-04-16',
   rules => [
     {
       id => '122a981f-5c80-4044-8c21-50174c2ce70f',
       provider => 'wernli@in2p3.fr',
-      patterns => [ 'promoted@ANYSTRING:promoted:@' ],
-      examples => [ { program => 'promoted', test_message => 'promoted message'} ],
+      patterns => [ 'this', 'this@ANYSTRING:promoted:@' ],
+      examples => [ { program => 'sshd', test_message => 'this is a message which we know very well'} ],
       ruleclass => 'promoted',
     }
   ]
@@ -21,14 +21,14 @@ patterndb::simple::ruleset { 'promoted':
 
 patterndb::simple::ruleset { 'generated':
   id => '5be2a930-7a0f-4ef4-9224-e49c4b27408b',
-  patterns => ['generated'],
+  patterns => ['sshd'],
   pubdate => '2019-04-16',
   rules => [
     {
       id => '9c094449-c96a-4e96-929b-b6c448360e64',
       provider => 'wernli@in2p3.fr',
-      patterns => [ 'generated@ANYSTRING:generated:@' ],
-      examples => [ { program => 'generated', test_message => 'generated message'} ],
+      patterns => [ 'that', 'that@ANYSTRING:generated:@' ],
+      examples => [ { program => 'sshd', test_message => 'that is an interesting message'} ],
       ruleclass => 'generated',
     }
   ]
